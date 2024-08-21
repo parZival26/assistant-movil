a3f72f6600584cc680f44b54514af866b5bcb1a3
Juan José Medina Clavijo <jjr20060326@gmail.com>
2024-08-21 13:29:12 -0500

    Pantalla y API a login terminados el token se guarda en local storage

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,100 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, TextInput, StyleSheet, Button } from "react-native";
+import { useState } from "react";
+import { login } from "../services/apiService";
+
+export default function LoginScreen() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+    const validateForm = () => {
+        let errors = {
+            username: '',
+            password: '',
+        };
+    
+        if (!username) {
+            errors.username = 'El usuario es requerido';
+        }
+    
+        if (!password) {
+            errors.password = 'La contraseña es requerida';
+        }
+    
+        setErrors(errors);
+        return Object.values(errors).every((error) => error === '');
+    }
+
+    const submitForm = () => {
+        
+        if (validateForm()) {
+            console.log('Formulario válido');
+            const result = login({ username, password });
+            result.then((response) => {
+                if (response.error) {
+                    alert(response.error);
+                } else {                    
+                    alert('Inicio de sesión exitoso');
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+            
+            
+        }
+    }
+
+    
+
+    return (
+        <SafeAreaView>
+            <Text style={styles.titulo}>Hola</Text>
+            <Text style={styles.subtitle}>Inicia sesión en tu cuenta</Text>
+            <TextInput
+                placeholder='Usuario'
+                style={styles.textInput}
+                onChangeText={text => setUsername(text)}
+                value={username}
+            />
+            {errors.username ? <Text style={styles.error}>{errors.username}</Text> : null}
+            <TextInput
+                placeholder='Contraseña'
+                style={styles.textInput}
+                onChangeText={text => setPassword(text)}
+                value={password}
+                secureTextEntry
+            />
+            {errors.password ? <Text style={styles.error}>{errors.password}</Text> : null}
+            <Button
+                onPress={() => submitForm()}
+                title='Iniciar Sesion'
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    titulo: {
+        fontSize: 70,
+        color: '#000',
+        fontWeight: 'bold'
+    },
+    subtitle: {
+        fontSize: 20,
+        color: 'gray',
+    },
+    textInput: {
+        padding: 6,
+        paddingStart: 20,
+        width: '80%',
+        height: 50,
+        marginVertical: 10,
+        borderRadius: 30,
+        backgroundColor: '#fff'
+    },
+    error: {
+        color: 'red',
+        margin: 5
+    }
+});
--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,3 +1,5 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 // src/services/apiService.ts
 const BASE_URL = 'http://tuquio.com/assistant/api';
 
@@ -9,6 +11,35 @@ interface Event {
     speaker: string;
     location: string;
   }
+
+
+interface Login {
+  username: string;
+  password: string;  
+}
+
+export const login  = async (login: Login) => {
+  try {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(login),
+    });
+    if (!response.ok) {
+      console.log('Error logging in:', response.status);
+      
+      return { error: 'Usuario o contraseña incorrectos' };
+    }
+    const data = await response.json();
+    await AsyncStorage.setItem('access', data.accessToken);
+    return data;
+  } catch (error) {
+    console.error('Error logging in:', error);
+    throw error;
+  }
+};
 
   export const createEvent = async (event: Event, token: string) => {
     try {
